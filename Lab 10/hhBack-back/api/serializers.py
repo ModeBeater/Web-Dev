53c3d35b189efcaecba032d8039a6287669f112a
ModeBeater <tashimov_nurh@mail.ru>
2023-04-14 18:15:38 +0600

    cbv and fbv completed

--- Lab 10/hhBack-back/api/serializers.py
+++ Lab 10/hhBack-back/api/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company,Vacancy
 class CompanySerializer1(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
-    description = serializers.CharField(max_length=1000)
-    city = serializers.CharField(max_length=255)
-    address = serializers.CharField(max_length=1000)
+    # description = serializers.CharField(max_length=1000)
+    # city = serializers.CharField(max_length=255)
+    # address = serializers.CharField(max_length=1000)
+    def create(self, validated_data):
+        company = Company.objects.create(**validated_data)
+        return company
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
 
 class VacancySerializer1(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -20,5 +27,7 @@ class CompanySerializer2(serializers.ModelSerializer):
         fields = ('id','name','description','city')
 
 
-class VacancySerializer(serializers.ModelSerializer):
-    pass
+class VacancySerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = ('id','name','description','salary')
